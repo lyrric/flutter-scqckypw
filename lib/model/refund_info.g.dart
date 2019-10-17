87c7217790c724f777b67f001e95c22c8f0ac08f
wangxiaodong <xx20510@163.com>
2019-10-17 18:29:52 +0800

    订单退款待完成

---
+++ lib/model/refund_info.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'refund_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RefundInfo _$RefundInfoFromJson(Map<String, dynamic> json) {
+  return RefundInfo()
+    ..fromStation = json['startName'] as String
+    ..targetStation = json['stopName'] as String
+    ..departureTime = json['startTime'] as String
+    ..ticketPrice = (json['price'] as num)?.toDouble()
+    ..refundPrice = (json['conut'] as num)?.toDouble();
+}
+
+Map<String, dynamic> _$RefundInfoToJson(RefundInfo instance) =>
+    <String, dynamic>{
+      'startName': instance.fromStation,
+      'stopName': instance.targetStation,
+      'startTime': instance.departureTime,
+      'price': instance.ticketPrice,
+      'conut': instance.refundPrice,
+    };
